85009f5dba07a76e58da4407bd91a8625df86893
MarioNadal <220056@iessanalberto.com>
2023-10-23 10:20:21 +0200

    Finalizado con comentarios Queda por resolver la concatenacion de String y int de la respuesta 23/10/2023

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import math
 
-
+# Función que devuelve el resultado de la ecuación
 def resolver():
     a = (int)(entradaA.get())
     b = (int)(entradaB.get())
@@ -10,31 +10,37 @@ def resolver():
         labelRespuesta["text"] = "No tiene soluciones reales"
     labelRespuesta["text"] = "Las soluciones son: ", int((-b + math.sqrt(b*b - 4 * a * c)) / (2 * a)), " y ", int((-b - math.sqrt(b*b - 4 * a * c)) / (2 * a))
 
-
+# Abrimos una ventana
 window = tk.Tk()
 window.title("Calculadora Ecuaciones 2º Grado")
 
+# Creamos una descripcion
 labelDescripcion = tk.Label(text="Programa para resolver ecuaciones de 2º grado de la forma ax^2 + bx + c = 0")
 labelDescripcion.grid(row=0, column=0)
 
+# Creamos la introducción de a
 labelIntroduceA = tk.Label(text="Introduce a: ")
 labelIntroduceA.grid(row=1, column=0)
 entradaA = tk.Entry()
 entradaA.grid(row=1, column=1)
 
+# Creamos la introducción de b
 labelIntroduceB = tk.Label(text="Introduce b: ")
 labelIntroduceB.grid(row=2, column=0)
 entradaB = tk.Entry()
 entradaB.grid(row=2, column=1)
 
+# Creamos la introducción de c
 labelIntroduceC = tk.Label(text="Introduce c: ")
 labelIntroduceC.grid(row=3, column=0)
 entradaC = tk.Entry()
 entradaC.grid(row=3, column=1)
 
+# Creamos el botón que nos resolverá la ecuación dando la respuesta
 buttonResolver = tk.Button(text="Resolver", command=resolver)
 buttonResolver.grid(row=4, column=0)
 
+# Creamos el texto que nos dará las respuestas de la ecuación
 labelRespuesta = tk.Label(text="")
 labelRespuesta.grid(row=5, column=0)
 
